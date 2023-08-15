5ace3f90d78feda657dca45f44ffc8a9015cedbc
Giovanny Putri Andini <86881191+giovannyptr@users.noreply.github.com>
2023-08-15 09:26:53 +0700

    pagination fixed

--- github-search/src/components/Pagination.js
+++ github-search/src/components/Pagination.js
@@ -1,26 +1,55 @@
 import React from 'react';
 
 const Pagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }) => {
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-  const pages = [];
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const hasPrevious = currentPage > 1;
+    const hasNext = currentPage < totalPages;
 
-  for (let i = 1; i <= Math.min(3, totalPages); i++) {
-    pages.push(i);
-  }
+    const generatePages = () => {
+        if (totalPages <= 3) {
+            return [...Array(totalPages).keys()].map(n => n + 1);
+        }
 
-  return (
-    <div className="pagination">
-      {pages.map(page => (
-        <button 
-          key={page}
-          className={page === currentPage ? 'active' : ''}
-          onClick={() => onPageChange(page)}
-        >
-          {page}
-        </button>
-      ))}
-    </div>
-  );
-}
+        if (currentPage === 1 || currentPage === 2) {
+            return [1, 2, 3];
+        } else if (currentPage === totalPages || currentPage === totalPages - 1) {
+            return [totalPages - 2, totalPages - 1, totalPages];
+        } else {
+            return [currentPage - 1, currentPage, currentPage + 1];
+        }
+    };
+
+    return (
+        <div className="pagination">
+            {hasPrevious && (
+                <button
+                    className="prev"
+                    onClick={() => onPageChange(currentPage - 1)}
+                >
+                    Previous
+                </button>
+            )}
+
+            {generatePages().map(page => (
+                <button
+                    key={page}
+                    className={page === currentPage ? 'active' : ''}
+                    onClick={() => onPageChange(page)}
+                >
+                    {page}
+                </button>
+            ))}
+
+            {hasNext && (
+                <button
+                    className="next"
+                    onClick={() => onPageChange(currentPage + 1)}
+                >
+                    Next
+                </button>
+            )}
+        </div>
+    );
+};
 
 export default Pagination;
